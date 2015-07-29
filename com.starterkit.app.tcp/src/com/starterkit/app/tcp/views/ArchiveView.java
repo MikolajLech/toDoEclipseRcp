174d087d39dc326babe00cec562f6ad49f000db7
Mikolaj Lech <mikolajj.lech@gmail.com>
2015-07-29 16:29:43 +0200

    Add menu with exit command.
    Signed-off-by: Mikolaj Lech <mikolajj.lech@gmail.com>

--- com.starterkit.app.tcp/src/com/starterkit/app/tcp/views/ArchiveView.java
+++ com.starterkit.app.tcp/src/com/starterkit/app/tcp/views/ArchiveView.java
@@ -1,15 +1,38 @@
 package com.starterkit.app.tcp.views;
 
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.*;
-import org.eclipse.jface.viewers.*;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.jface.action.*;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuListener;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.IToolBarManager;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.*;
-import org.eclipse.swt.widgets.Menu;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IWorkbenchActionConstants;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.ViewPart;
 
 
 /**
@@ -41,6 +64,7 @@ public class ArchiveView extends ViewPart {
 	private Action action1;
 	private Action action2;
 	private Action doubleClickAction;
+	private Table table_1;
 
 	/*
 	 * The content provider class is responsible for
@@ -51,6 +75,7 @@ public class ArchiveView extends ViewPart {
 	 * it and always show the same content 
 	 * (like Task List, for example).
 	 */
+
 	 
 	class ViewContentProvider implements IStructuredContentProvider {
 		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
@@ -87,11 +112,58 @@ public class ArchiveView extends ViewPart {
 	 * to create the viewer and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
-		viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new FormLayout());
+		viewer = new TableViewer(composite, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		Table table = viewer.getTable();
+		FormData fd_table = new FormData();
+		fd_table.bottom = new FormAttachment(0, 293);
+		fd_table.right = new FormAttachment(0, 128);
+		fd_table.top = new FormAttachment(0, 3);
+		fd_table.left = new FormAttachment(0, 3);
+		table.setLayoutData(fd_table);
+		
+		table_1 = new Table(composite, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+		FormData fd_table_1 = new FormData();
+		fd_table_1.bottom = new FormAttachment(0, 333);
+		fd_table_1.right = new FormAttachment(0, 433);
+		fd_table_1.top = new FormAttachment(0, 3);
+		fd_table_1.left = new FormAttachment(0, 134);
+		table_1.setLayoutData(fd_table_1);
+		table_1.setHeaderVisible(true);
+		table_1.setLinesVisible(true);
 		viewer.setContentProvider(new ViewContentProvider());
 		viewer.setLabelProvider(new ViewLabelProvider());
 		viewer.setSorter(new NameSorter());
 		viewer.setInput(getViewSite());
+		String[] titles = {"num", "name", "priority", "finished"};
+		int colNum = titles.length;
+		for(int i = 0; i < colNum; i++) {
+			TableColumn column = new TableColumn(table_1, SWT.NONE);
+			if(i == 0){
+				column.setWidth(40);
+			}
+			else if(i == 3) {
+				column.setWidth(70);
+			}
+			else {
+				column.setWidth(90);
+			}
+			column.setText(titles[i]);
+		}
+		int itemNum = 3;
+		for (int i=0; i<itemNum; i++) {
+			TableItem item = new TableItem (table_1, SWT.NONE);
+			item.setText (0, "" + table_1.getItemCount());
+			item.setText (1, "nowe zadanie");
+			item.setText (2, "mało ważne");
+		}
+		for (int i=0; i<titles.length; i++) {
+//			table_1.getColumn(i).pack();
+			table_1.getColumn(i).setMoveable(true);
+		}
+		
 
 		// Create the help context id for the viewer's control
 		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(), "com.starterkit.app.tcp.viewer");
@@ -99,6 +171,7 @@ public class ArchiveView extends ViewPart {
 		hookContextMenu();
 		hookDoubleClickAction();
 		contributeToActionBars();
+		
 	}
 
 	private void hookContextMenu() {
@@ -166,6 +239,8 @@ public class ArchiveView extends ViewPart {
 			}
 		};
 	}
+	
+	
 
 	private void hookDoubleClickAction() {
 		viewer.addDoubleClickListener(new IDoubleClickListener() {
